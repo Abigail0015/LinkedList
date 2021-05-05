7156344aac9a8314f86ac8e8366361e30fec3105
Abigail0015 <A298360@alumnos.uaslp.mx>
2021-05-04 19:10:34 -0500

    UNIT TESTING

--- test/uaslp/ingenieria/labs/list/LinkedListTest.java
+++ test/uaslp/ingenieria/labs/list/LinkedListTest.java
@@ -1,5 +1,25 @@
 package uaslp.ingenieria.labs.list;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LinkedListTest
 {
+    @Test
+    public void whenANewListIsCreated_ThenListIsEmpty()
+    {
+        ArrayList<Integer> lista = new ArrayList<>();
+        assertEquals(0,lista.getSize());
+    }
+    @Test
+    public void  whenANewListIsCreated_sizeIsZero()
+    {
+        int size;
+
+        ArrayList<Integer> lista = new ArrayList<>();
+        size = lista.getSize();
+        assertEquals(0,size);
+    }
+
 }
